e34e4f1d6f883479cb54b9169d683f03f1bf52fa
ijacobsen <ian.jacobsen.92@gmail.com>
2022-07-18 13:10:06 -0400

    functionally mostly working in console player;

--- javascript.js
+++ javascript.js
@@ -39,6 +39,18 @@ maker.
 */
 
 
+var playButton = document.getElementById('playButton');
+playButton.addEventListener('click', playGame);
+function playGame() {
+    let gC = gameController;
+    gC.newGame();
+}
+
+// define a player as a factory
+const playerFactory = (name) => {
+    let symbol = name;
+    return {name, symbol}
+}
 
 // define a gameBoard as a module
 const gameBoard = (() => {
@@ -50,25 +62,29 @@ const gameBoard = (() => {
     const markSquare = function (player, position) {
         let row = position[0];
         let col = position[1];
-        console.log(board[row][col]);
         if (board[row][col] === null) {
             board[row][col] = player.symbol;
         }
-        console.log(board);
+        //console.log(board);
     }
 
     const getSquareVal = function (idx) {
         let row = parseInt((idx/3));
         let col = idx%3;
         let squareVal = board[row][col];
-        console.log(`square val of idx:${idx} is ${squareVal}`);
+        //console.log(`square val of idx:${idx} is ${squareVal}`);
         if (squareVal === null){
             squareVal = '';
         }
         return squareVal;
     }
 
-    return {markSquare, getSquareVal}
+    const reportBoard = function () {
+        console.log('=====================')
+        console.log(board);
+    }
+
+    return {markSquare, getSquareVal, reportBoard}
 })();
 
 // define a displayController as a module
@@ -99,16 +115,152 @@ const displayController = (() => {
     return {renderBoard}
 })();
 
-// define a player as a factory
-const playerFactory = (name) => {
-    let symbol = name;
-    return {name, symbol}
-}
+// define gameController
+const gameController = (() => {
+
+    // start game
+    const newGame = function () {
+
+        // get player 1 name
+        let p_1_name = 'joe';
+        let p_1 = playerFactory(p_1_name);
+
+        // get player 2 name
+        let p_2_name = 'mary'
+        let p_2 = playerFactory(p_2_name);
+    
+        // render empty gameboard
+        let gBoard = gameBoard;
+
+        // render a display controller
+        let dController = displayController;
+
+        // play round
+        let winner = playRound(p_1, p_2, gBoard, dController);
+
+        // display game winner
+        if (winner != null){
+            console.log(`${winner.name} wins!`);
+        }
+        else {
+            console.log('its a tie');
+        }
+    }
+
+    // play a round
+    const playRound = function (p_1, p_2, gBoard, dController) {
+
+        const checkEnd = function(gBoard) {
+
+            // check horizontal lines
+            for (let i=0; i<3; i++){
+                if (gBoard.getSquareVal(i*3) !== ''){
+                    if (gBoard.getSquareVal(i*3) === gBoard.getSquareVal(i*3+1)) {
+                        if (gBoard.getSquareVal(i*3) === gBoard.getSquareVal(i*3+2)) {
+                            let winner = gBoard.getSquareVal(i*3);
+                            if (winner !== '') {
+                                return winner;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // check vertical lines
+            for (let i=0; i<3; i++){
+                if (gBoard.getSquareVal(i) !== ''){
+                    if (gBoard.getSquareVal(i) === gBoard.getSquareVal(i+3)) {
+                        if (gBoard.getSquareVal(i+3) === gBoard.getSquareVal(i+6)) {
+                            let winner = gBoard.getSquareVal(i);
+                            if (winner !== '') {
+                                return winner;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // check diagonal lines
+            if (gBoard.getSquareVal(4) !== ''){
+
+                // 0 4 8
+                if (gBoard.getSquareVal(0) === gBoard.getSquareVal(4)){
+                    if (gBoard.getSquareVal(0) === gBoard.getSquareVal(8)){
+                        let winner = gBoard.getSquareVal(0);
+                        if (winner !== ''){
+                            return winner;
+                        }
+                    }
+                }
+
+                // 2 4 6
+                if (gBoard.getSquareVal(2) === gBoard.getSquareVal(4)){
+                    if (gBoard.getSquareVal(2) === gBoard.getSquareVal(6)){
+                        let winner = gBoard.getSquareVal(2);
+                        if (winner !== ''){
+                            return winner;
+                        }
+                    }
+                }
+            }
+
+            return '';
+        }
+
+        const makeMove = function(obj) {
+            let move = window.prompt(obj.player.name + ', whats your move? x,y', '');
+            obj.board.markSquare(obj.player, [move[0], move.slice(-1)]);
+            obj.display.renderBoard(obj.board);
+        }
+
+        let checkResult = null;
+        let obj = null;
+
+        for (let i=0; i<9; i++) {
+
+            // player 1 move
+            obj = {player: p_1, board: gBoard, display: dController};
+            makeMove(obj);
+        
+            // check if game over
+            checkResult = checkEnd(gBoard);
+            if (checkResult !== '') {
+                return p_1;
+            }
+
+            // player 2 move
+            obj = {player: p_2, board: gBoard, display: dController};
+            makeMove(obj);
+        
+            // check if game over
+            checkResult = checkEnd(gBoard);
+            if (checkResult !== '') {
+                return p_2;
+            }
+
+        }
+
+        // if no one won then return a tie
+        return null;
+
+    }
+
+    // restart game 
+
+    // public members
+    return {newGame}
+})();
+
 
 /*
 let joe = playerFactory('joe');
 let board = gameBoard;
 gameBoard.markSquare(joe, [0, 0]);
-let controller = displayController;
-controller.renderBoard(board);
+let dController = displayController;
+dController.renderBoard(board);
+*/
+
+/*
+let gC = gameController;
+gC.newGame();
 */
